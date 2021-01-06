a023e207838b9231a95ff3f4d853230045fb424a
Gabriel Carneiro <71238933+gacarneirojr@users.noreply.github.com>
2021-01-06 14:04:12 -0300

    changes on contact area - changing innerHTML javaScript

--- js/main.js
+++ js/main.js
@@ -7,11 +7,13 @@ let hover4 = document.getElementById("aprisco");
 let hoover = (a) => {
     a.style.transform = 'scale(1.1)';
     a.style.transition = 'all .5s';
+    a.style.border = '2px solid yellow'
 }
 
 let mouseOut = (a) => {
     a.style.transform = 'scale(1.0)';
     a.style.transition = 'all .5s';
+    a.style.border = ''
 }
 
 hover1.onmouseover = function () {hoover(hover1)};
@@ -26,9 +28,38 @@ hover3.onmouseout = function () {mouseOut(hover3)};
 hover4.onmouseover = function () {hoover(hover4)};
 hover4.onmouseout = function () {mouseOut(hover4)};
 
+let github = document.getElementById('github')
 
-/*
-let hoverAll = document.querySelectorAll('.grid')
+let change1 = (b) => {
+    b.innerHTML = 'github.com/gacarneirojr';
+}
+let undo1 = (b) => {
+    b.innerHTML = 'github';
+}
 
-hoverAll.onmouseover = function () {hoover(hoverAll)};
-hoverAll.onmouseout = function () {mouseOut(hoverAll)};*/
+github.onmouseover = function () {change1(github)};
+github.onmouseout = function () {undo1(github)};
+
+let linkedin = document.getElementById('linkedin')
+
+let change2 = (b) => {
+    b.innerHTML = 'linkedin.com/in/gabriel-araujo-carneiro-junior-618770102/'
+}
+let undo2 = (b) => {
+    b.innerHTML = 'linkedin';
+}
+
+linkedin.onmouseover = function () {change2(linkedin)};
+linkedin.onmouseout = function () {undo2(linkedin)};
+
+let email = document.getElementById('email')
+
+let change3 = (b) => {
+    b.innerHTML = 'gacarneirojrx@gmail.com'
+}
+let undo3 = (b) => {
+    b.innerHTML = 'e-mail';
+}
+
+email.onmouseover = function () {change3(email)};
+email.onmouseout = function () {undo3(email)};

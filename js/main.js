9ea28fc1760d9bb6c891c567ffd76096c734fb89
Gabriel Carneiro <71238933+gacarneirojr@users.noreply.github.com>
2021-01-06 11:22:07 -0300

    javaScript added to mouseOver and mouseOut portfolio

---
+++ js/main.js
@@ -0,0 +1,28 @@
+
+let hover1 = document.getElementById("amae");
+let hover2 = document.getElementById("photomatic");
+let hover3 = document.getElementById("tea");
+let hover4 = document.getElementById("aprisco");
+
+
+let hoover = (a) => {
+    a.style.transform = 'scale(1.1)';
+    a.style.transition = 'all .5s';
+}
+
+let mouseOut = (a) => {
+    a.style.transform = 'scale(1.0)';
+    a.style.transition = 'all .5s';
+}
+
+hover1.onmouseover = function () {hoover(hover1)};
+hover1.onmouseout = function () {mouseOut(hover1)};
+
+hover2.onmouseover = function () {hoover(hover2)};
+hover2.onmouseout = function () {mouseOut(hover2)};
+
+hover3.onmouseover = function () {hoover(hover3)};
+hover3.onmouseout = function () {mouseOut(hover3)};
+
+hover4.onmouseover = function () {hoover(hover4)};
+hover4.onmouseout = function () {mouseOut(hover4)};
